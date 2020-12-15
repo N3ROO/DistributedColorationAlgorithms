57e1a99e904828ee5e91114e8df64823ce1bb49a
N3ROO <swellchaser33@gmail.com>
2020-12-15 17:01:09 +0100

    added IntelliJ project files with JBotSim working

---
+++ src/main/java/dev/gallon/Main.java
@@ -0,0 +1,12 @@
+package dev.gallon;
+
+import io.jbotsim.core.Topology;
+import io.jbotsim.ui.JViewer;
+
+public class Main {
+    public static void main(String[] args) {
+        Topology tp = new Topology();
+        new JViewer(tp);
+        tp.start();
+    }
+}
