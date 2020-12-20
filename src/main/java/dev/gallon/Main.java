a83534ccd7c4eac7633fe50e4a06e4d3934a84ed
N3ROO <swellchaser33@gmail.com>
2020-12-20 14:18:09 +0100

    implemented greedy algorithm

--- src/main/java/dev/gallon/Main.java
+++ src/main/java/dev/gallon/Main.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Algorithm> algorithms = new ArrayList<>();
+        // algorithms.add(new GreedyAlgorithm());
         algorithms.add(new Algorithm(CycleColorationNode.class, "Coloration de cycle"));
 
         new Window(algorithms);
---
+++ src/main/java/dev/gallon/algorithms/GreedyAlgorithm.java
@@ -0,0 +1,23 @@
+package dev.gallon.algorithms;
+
+import io.jbotsim.core.Color;
+import io.jbotsim.core.Node;
+
+public class GreedyAlgorithm extends Algorithm {
+
+    public GreedyAlgorithm() {
+        super(Node.class, "Greedy");
+
+        jviewer.getJTopology().getTopology().addStartListener(() -> {
+            for (int round = 1; round <= 4; round++) {
+                for (Node node : jviewer.getJTopology().getTopology().getNodes()) {
+                    int newId = Utility.firstFree(node.getNeighbors(), node, 0, Integer.MAX_VALUE);
+                    node.setColor(Utility.getColorFromInt(newId));
+                    node.setID(newId);
+                }
+            }
+        });
+    }
+
+
+}
