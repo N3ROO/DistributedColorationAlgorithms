6859f77dc4dc5df308b7cbfe0f2013af9e6b17b9
N3ROO <swellchaser33@gmail.com>
2020-12-22 16:32:15 +0100

    removed raw color6 algorithm

--- src/main/java/dev/gallon/Main.java
+++ src/main/java/dev/gallon/Main.java
@@ -1,7 +1,6 @@
 package dev.gallon;
 
 import dev.gallon.algorithms.Algorithm;
-import dev.gallon.algorithms.ColorSixNode;
 import dev.gallon.algorithms.CycleColorationNode;
 import dev.gallon.algorithms.GreedyAlgorithm;
 import dev.gallon.ui.Window;
@@ -12,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Algorithm> algorithms = new ArrayList<>();
         algorithms.add(new GreedyAlgorithm("Greedy (hors projet)"));
-        algorithms.add(new Algorithm(ColorSixNode.class, "Color6 (hors projet)"));
         algorithms.add(new Algorithm(CycleColorationNode.class, "Coloration de cycle (q1)"));
 
         new Window(algorithms);
