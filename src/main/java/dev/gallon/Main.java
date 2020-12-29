a4e01e07326eb9583569a39d1b36cc6d80fa784b
N3ROO <swellchaser33@gmail.com>
2020-12-29 17:32:56 +0100

    added arbitrary coloration node

--- src/main/java/dev/gallon/Main.java
+++ src/main/java/dev/gallon/Main.java
@@ -1,6 +1,7 @@
 package dev.gallon;
 
 import dev.gallon.algorithms.Algorithm;
+import dev.gallon.algorithms.ArbitraryColorationNode;
 import dev.gallon.algorithms.CycleColorationNode;
 import dev.gallon.algorithms.GreedyAlgorithm;
 import dev.gallon.ui.Window;
@@ -12,6 +13,7 @@ public class Main {
         ArrayList<Algorithm> algorithms = new ArrayList<>();
         algorithms.add(new GreedyAlgorithm("Greedy (hors projet)"));
         algorithms.add(new Algorithm(CycleColorationNode.class, "Coloration de cycle (q1)"));
+        algorithms.add(new Algorithm(ArbitraryColorationNode.class, "Coloration de graphe arbitraire (q2)"));
 
         new Window(algorithms);
     }
---
+++ src/main/java/dev/gallon/algorithms/ArbitraryColorationNode.java
@@ -0,0 +1,16 @@
+package dev.gallon.algorithms;
+
+import io.jbotsim.core.Node;
+
+public class ArbitraryColorationNode extends Node {
+
+    @Override
+    public void onStart() {
+        // TODO
+    }
+
+    @Override
+    public void onClock() {
+        // TODO
+    }
+}
